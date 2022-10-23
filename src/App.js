6d7963daa8519e352e1b7daf05a75c924724c64a
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2022-10-23 12:15:36 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import uuid from 'react-uuid'
+import steps from './data.json'
 
-function App() {
+export default function App() {
+  const [trigger, setTrigger] = useState(steps[0].id)
+  const [userMessage, setUserMessage] = useState("")
+  const [allMessages, setAllMessages] = useState([])
+  const inputRef = useRef(null)
+  const currentStepData = steps.find(({ id }) => id === trigger)
+
+  const handleChange = (event) => {
+    setUserMessage(event.target.value)
+  }
+
+  const addMessage = (event) => {
+    event.preventDefault()
+    setAllMessages(prev => [...prev, userMessage])
+    setTrigger(currentStepData.trigger)
+    // setUserMessage("")
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    if (currentStepData.user) return
+    setTimeout(() => {
+      setAllMessages(prev => [...prev, currentStepData.message])
+      setTrigger(currentStepData.trigger)
+    }, 1000)
+  }, [currentStepData])
+  console.log(trigger)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {allMessages.map(message => <p key={uuid()}>{message}</p>)}
+      <form>
+        <input ref={inputRef} type="text" name="todo" value={userMessage} onChange={handleChange} />
+        <button onClick={addMessage}>send</button>
+      </form>
     </div>
-  );
+  )
 }
-
-export default App;
